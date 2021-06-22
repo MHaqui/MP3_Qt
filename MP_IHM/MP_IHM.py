7108098187585892b04c0815158d23f3dfdf4dd7
Maxime Haquin <maxime.haquinlage@gmail.com>
2021-06-22 16:55:15 +0200

    Première interface

--- MP_IHM/MP_IHM.py
+++ MP_IHM/MP_IHM.py
@@ -8,6 +8,67 @@ if __name__ == '__main__' :
 
 
     app = QApplication(sys.argv)
-    layout = QVBoxLayout()
-    but1 = QPushButton()
-    but2 = QPushButton()
+
+    image = QPushButton("non available")
+
+    song_name = QLineEdit("Afraid of everyone")
+    song_artist = QLineEdit("The national")
+    song_album = QLineEdit("High Violet")
+
+    song_1 = QLineEdit("1. Terrible Love")
+    song_2 = QLineEdit("2. Sorrow")
+    song_3 = QLineEdit("3. Anyone's Ghost")
+    song_4 = QLineEdit("4. Little Faith")
+    song_5 = QLineEdit("1. Arfraid of Evryone")
+    song_6 = QLineEdit("6. Bloodbuzz Ohio")
+
+    but_skip = QPushButton(">>")
+    but_play = QPushButton(">")
+    but_restart = QPushButton("<<")
+    but_sound = QPushButton("mute")
+
+    layout_button = QHBoxLayout()
+    layout_button.addWidget(but_skip)
+    layout_button.addWidget(but_play)
+    layout_button.addWidget(but_restart)
+
+
+    slayout1 = QVBoxLayout()
+    slayout1.addWidget(image)
+
+    slayout2 = QVBoxLayout()
+    slayout2.addWidget(song_name)
+    slayout2.addWidget(but_sound)
+    slayout2.addWidget(song_artist)
+    slayout2.addWidget(song_album)
+    slayout2.addLayout(layout_button)
+    
+
+    slayout3 = QVBoxLayout()
+    slayout3.addWidget(song_1)
+    slayout3.addWidget(song_2)
+    slayout3.addWidget(song_3)
+    slayout3.addWidget(song_4)
+    slayout3.addWidget(song_5)
+    slayout3.addWidget(song_6)
+
+
+
+    layout = QHBoxLayout()
+    layout.addLayout(slayout1)
+    layout.addLayout(slayout2)
+    layout.addLayout(slayout3)
+    
+
+    widget = QWidget()
+    widget.setLayout(layout)
+    widget.move(600,400)
+    widget.resize(500,200)
+    widget.setMinimumWidth(150)
+    widget.setMaximumWidth(1000)
+    widget.setMinimumHeight(150)
+    widget.setMaximumHeight(1000)
+    widget.setWindowTitle("musique")
+
+    widget.show()
+    app.exec_() 
