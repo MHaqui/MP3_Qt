7c205a30d989f79b1bfefd78c107dfdb7d274fcb
Maxime Haquin <maxime.haquinlage@gmail.com>
2021-06-22 15:31:44 +0200

    Premier Code

---
+++ MP_IHM/MP_IHM.py
@@ -0,0 +1,13 @@
+import PySide2
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+from PySide2.QtCore import *
+import sys
+
+if __name__ == '__main__' :
+
+
+    app = QApplication(sys.argv)
+    layout = QVBoxLayout()
+    but1 = QPushButton()
+    but2 = QPushButton()
