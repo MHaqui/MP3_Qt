1a91db7e9ad5311b9d82bfdc23daddb415f4fa58
Maxime Haquin <maxime.haquinlage@gmail.com>
2021-06-28 10:18:02 +0200

    Amélioration code

--- MP_IHM/MP_IHM.py
+++ MP_IHM/MP_IHM.py
@@ -12,17 +12,18 @@ class MyWidget(QWidget):
         #blue = QColor()
         #blue.setNamedColor("blue")
 
-        #self.setAttribute(Qt.WA_TranslucentBackground, True)
         self.setWindowFlag(Qt.FramelessWindowHint)
         self.pixmap = QPixmap()
         self.pixmap.load("C:/Users/maxim/Pictures/Saved Pictures/Tigre")
         self.pixmap = self.pixmap.scaled(100,100)
 
-        self.song_name = QLabel("Afraid of everyone")
+        self.song_name = QLabel("2. Sorrow")
         self.song_artist = QLabel("The national")
         self.song_album = QLabel("High Violet")
+        self.rating = QLabel("Rating : 4/5")
 
-        self.i = 3
+        self.ind = 1
+        self.run = True
 
         self.song_1 = QPushButton("1. Terrible Love")
         self.song_2 = QPushButton("2. Sorrow")
@@ -30,23 +31,23 @@ class MyWidget(QWidget):
         self.song_4 = QPushButton("4. Little Faith")
         self.song_5 = QPushButton("5. Arfraid of Evryone")
         self.song_6 = QPushButton("6. Bloodbuzz Ohio")
-        self.rating = QLabel("Rating : 4/5")
+        
+        self.list_songs = []
+        self.list_songs.append(self.song_1)
+        self.list_songs.append(self.song_2)
+        self.list_songs.append(self.song_3)
+        self.list_songs.append(self.song_4)
+        self.list_songs.append(self.song_5)
+        self.list_songs.append(self.song_6)
 
-        self.songs = QListWidget()
-        self.songs.addItem("1. Terrible Love")
-        self.songs.addItem("2. Sorrow")
-        self.songs.addItem("3. Anyone's Ghost")
-        self.songs.addItem("4. Little Faith")
-        self.songs.addItem("5. Arfraid of Evryone")
-        self.songs.addItem("6. Bloodbuzz Ohio")
-        self.songs.setItemWidget(self.songs.item(0), self.song_1)
-        self.songs.setItemWidget(self.songs.item(1), self.song_2)
-        self.songs.setItemWidget(self.songs.item(2), self.song_3)
-        self.songs.setItemWidget(self.songs.item(3), self.song_4)
-        self.songs.setItemWidget(self.songs.item(4), self.song_5)
-        self.songs.setItemWidget(self.songs.item(5), self.song_6)
-        self.songs.setFixedSize(150,100)
-
+        self.Wsongs = QListWidget()
+        for i in range(6):
+            self.Wsongs.addItem(self.list_songs[i].text())
+            self.Wsongs.setItemWidget(self.Wsongs.item(i), self.list_songs[i])
+            self.list_songs[i].setStyleSheet("background-color : white")
+            
+        self.Wsongs.setFixedSize(150,100)
+        self.list_songs[1].setStyleSheet("background-color : grey")
 
         self.but_skip = QPushButton(">>")
         self.but_play = QPushButton(">")
@@ -82,7 +83,7 @@ class MyWidget(QWidget):
         layout1.setContentsMargins(110,0,0,0)
 
         layout2 = QVBoxLayout()
-        layout2.addWidget(self.songs)
+        layout2.addWidget(self.Wsongs)
         layout2.setSpacing(0)
 
         layout = QHBoxLayout()
@@ -91,45 +92,77 @@ class MyWidget(QWidget):
         layout.addLayout(layout2)
 
         self.setLayout(layout)
+        x = 0
 
         #Actions boutons
 
-        self.song_1.clicked.connect(self.setSong_1)
+        self.list_songs[0].clicked.connect(self.setSong_1)
         self.song_2.clicked.connect(self.setSong_2)
         self.song_3.clicked.connect(self.setSong_3)
         self.song_4.clicked.connect(self.setSong_4)
         self.song_5.clicked.connect(self.setSong_5)
         self.song_6.clicked.connect(self.setSong_6)
         self.but_skip.clicked.connect(self.NextSong)
+        self.but_restart.clicked.connect(self.BefSong)
+        self.but_play.clicked.connect(self.Play)
         
         # Méthodes
+
+    def Play(self):
+        if self.run :
+            self.run = False
+            print("Stop")
+        else:
+            self.run = True
+            print("Play")
+
+    def BefSong(self):
+        self.list_songs[self.ind].setStyleSheet("background-color : white")
+        self.ind = (self.ind-1)%6
+        self.song_name.setText(self.Wsongs.item(self.ind).text())
+        self.list_songs[self.ind].setStyleSheet("background-color : grey")
+        
     def NextSong(self):
-        self.i = self.i+1
-        self.song_name.setText(self.songs.item(self.i%6).text())
+        self.list_songs[self.ind].setStyleSheet("background-color : white")
+        self.ind = (self.ind+1)%6
+        self.song_name.setText(self.Wsongs.item(self.ind).text())
+        self.list_songs[self.ind].setStyleSheet("background-color : grey")
     
-    def setSong_1 (self):
-        self.i = 0
-        self.song_name.setText(self.song_1.text())
+    def setSong_1(self, x):
+        self.list_songs[self.ind].setStyleSheet("background-color : white")
+        self.ind = 0
+        self.song_name.setText(self.list_songs[self.ind].text())
+        self.list_songs[self.ind].setStyleSheet("background-color : grey")
 
     def setSong_2 (self):
-        self.i = 1
+        self.list_songs[self.ind].setStyleSheet("background-color : white")
+        self.ind = 1
         self.song_name.setText(self.song_2.text())
+        self.list_songs[self.ind].setStyleSheet("background-color : grey")
 
     def setSong_3 (self):
-        self.i = 2
+        self.list_songs[self.ind].setStyleSheet("background-color : white")
+        self.ind = 2
         self.song_name.setText(self.song_3.text())
+        self.list_songs[self.ind].setStyleSheet("background-color : grey")
 
     def setSong_4 (self):
-        self.i = 3
-        self.song_name.setText(self.song_5.text())
+        self.list_songs[self.ind].setStyleSheet("background-color : white")
+        self.ind = 3
+        self.song_name.setText(self.song_4.text())
+        self.list_songs[self.ind].setStyleSheet("background-color : grey")
 
     def setSong_5 (self):
-        self.i = 4
+        self.list_songs[self.ind].setStyleSheet("background-color : white")
+        self.ind = 4
         self.song_name.setText(self.song_5.text())
+        self.list_songs[self.ind].setStyleSheet("background-color : grey")
 
     def setSong_6 (self):
-        self.i = 5
+        self.list_songs[self.ind].setStyleSheet("background-color : white")
+        self.ind = 5
         self.song_name.setText(self.song_6.text())
+        self.list_songs[self.ind].setStyleSheet("background-color : grey")
         
 
     def paintEvent(self, event : QPaintEvent) :
